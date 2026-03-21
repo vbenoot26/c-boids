052725f33c25cc10abed7e06d179ef6b0a52c9a4
vincent <vincent.benoot@gmail.com>
2026-03-21 14:34:17 +0100

    creates a moving bal

--- main.c
+++ main.c
@@ -1,16 +1,39 @@
 #include "raylib.h"
 
+struct Bal {
+  Vector2 location;
+  Vector2 speed;
+};
+
+void draw(struct Bal model) {
+  BeginDrawing();
+
+  ClearBackground(BLACK);
+  DrawCircle(model.location.x, model.location.y, 20, WHITE);
+
+  EndDrawing();
+}
+
 int main() {
   const int screenWidth = 800;
   const int screenHeight = 600;
-  InitWindow(screenWidth, screenHeight, "Raylib basic window");
+
+  InitWindow(screenWidth, screenHeight, "CIRCLE");
   SetTargetFPS(60);
+
+  struct Bal bal = {{200.0, 200.0}, {10.0, 10.0}};
+
   while (!WindowShouldClose()) {
-    BeginDrawing();
-    ClearBackground(RAYWHITE);
-    DrawText("It works!", 20, 20, 20, BLACK);
-    EndDrawing();
+
+    bal.location.x += bal.speed.x;
+    bal.location.x = (int) bal.location.x % screenWidth;
+    bal.location.y += bal.speed.y;
+    bal.location.y = (int) bal.location.y % screenHeight;
+    
+    draw(bal);
   }
   CloseWindow();
   return 0;
 }
+
+
