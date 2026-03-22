b03b19e4cdb2d54b564cf7e924c342275e98dd34
vincent <vincent.benoot@gmail.com>
2026-03-22 17:01:18 +0100

    adds separator calculation

--- calculator.c
+++ calculator.c
@@ -11,26 +11,44 @@ float getDistance(const struct Boid boid1, const struct Boid boid2) {
               (loc1.y - loc2.y) * (loc1.y - loc2.y));
 }
 
-int getNeighbours(const struct Boid boid,
-                  struct Boid *allboids, int boidsAmount,
-                  struct Boid *neighbourlist, int maxNeighbours) {
-  int neighbourAmount = 0;
-  int neighbourIndex = 0;
+void getNeighbours(const struct Context ctx, const struct Boid boid,
+                   struct Boid *allboids, int boidsAmount,
+                   struct Boid *neighbourlist, int *neighbourAmount,
+                   int maxNeighbours, struct Boid *separatorlist,
+                   int *separatorAmount) {
+  *neighbourAmount = 0;
+  *separatorAmount = 0;
 
   for (int i = 0; i < boidsAmount; i++) {
-    if (getDistance(boid, allboids[i]) < 50) {
+    float dist = getDistance(boid, allboids[i]);
+    if (dist < ctx.visionDistance) {
+      neighbourlist[*neighbourAmount] = allboids[i];
+
       neighbourAmount++;
 
-      neighbourlist[neighbourIndex] = allboids[i];
+      if (*neighbourAmount > maxNeighbours) {
+        return;
+      }
+    }
+
+    if (dist < ctx.separationDistance) {
+      separatorlist[*separatorAmount] = allboids[i];
+
+      separatorAmount++;
+
+      if (*separatorAmount > maxNeighbours) {
+        return;
+      }
     }
   }
-
-  return neighbourAmount;
 }
 
-void update(struct Context ctx, struct Boid *boid, struct Boid *neighbours, int neighbourAmount) {
-      boid->location.x += boid->speed.x;
-      boid->location.x = (int)boid->location.x % ctx.screenWidth;
-      boid->location.y += boid->speed.y;
-      boid->location.y = (int)boid->location.y % ctx.screenHeight;
+void update(struct Context ctx, struct Boid *boid,
+            struct Boid *neighbours, int neighbourAmount,
+            struct Boid *separators, int separatorAmount
+          ) {
+  boid->location.x += boid->speed.x;
+  boid->location.x = (int)boid->location.x % ctx.screenWidth;
+  boid->location.y += boid->speed.y;
+  boid->location.y = (int)boid->location.y % ctx.screenHeight;
 }
--- calculator.h
+++ calculator.h
@@ -1,10 +1,15 @@
 #ifndef CALC_H
 #define CALC_H
 
-int getNeighbours(struct Boid boid,
-                  struct Boid *allboids, int boidsAmount,
-                  struct Boid *neighbourlist, int maxNeighbours);
+void getNeighbours(const struct Context ctx, const struct Boid boid,
+                   struct Boid *allboids, int boidsAmount,
+                   struct Boid *neighbourlist, int *neighbourAmount,
+                   int maxNeighbours, struct Boid *separatorlist,
+                   int *separatorAmount);
 
-void update(struct Context ctx, struct Boid *boid, struct Boid *neighbours, int neighbourAmount);
+void update(struct Context ctx, struct Boid *boid,
+            struct Boid *neighbours, int neighbourAmount,
+            struct Boid *separators, int separatorAmount
+          );
 
 #endif
